41505dc8052aaf2aa42916caea52d4cdc6f1a52d
André Michelle <andre.michelle@gmail.com>
2024-11-18 14:18:59 +0100

    static php

--- playlist.php
+++ playlist.php
@@ -1,34 +1,43 @@
-<?php
-	
-	define( 'CRLF', "\r\n" );
-	//define( 'HOME', dirname( __FILE__ ).'/' );
-	
-	//-- security checks
-	if ( !is_dir( $_REQUEST[ 'folder' ] ) )
-		die( 'Need correct directory' );
-	//if ( str_replace( '../', '', $_REQUEST[ 'folder' ] ) != $_REQUEST[ 'folder' ] )
-	//	die( 'Nice try.' );
 
-	//-- read directory
-	//$d = dir( HOME.$_REQUEST[ 'folder' ] );
-	$d = dir( $_REQUEST[ 'folder' ] );
-	$p = ( substr( $_REQUEST[ 'folder' ], -1, 1 ) != '/' ) ? $_REQUEST[ 'folder' ].'/' : $_REQUEST[ 'folder' ];
-	$f = array();
-	
-	//-- get files
-	while ( false !== ( $file = $d->read() ) )
-		if ( strtolower( substr( $file, -3, 3 ) ) == 'mod' )
-			$f[ $file ] = strtolower( $file );
-			
-	//-- sort
-	array_multisort( $f, SORT_STRING, SORT_ASC );
-	reset( $f );
-	
-	//-- output xml
-	echo '<playlist>'.CRLF;
-	foreach ( $f as $file => $val )
-		echo chr( 9 ).'<song url="'.$p.$file.'" hasCredits="true" />'.CRLF;
-	echo '</playlist>';
-
-	$d->close();
-?>
+<playlist>
+	<song url="./mod/-super_mario_land.mod" hasCredits="true" />
+	<song url="./mod/Alloyrun.mod" hasCredits="true" />
+	<song url="./mod/C64 Mix.mod" hasCredits="true" />
+	<song url="./mod/cartoon-chips83.mod" hasCredits="true" />
+	<song url="./mod/chris-stop_for_a_minute.mod" hasCredits="true" />
+	<song url="./mod/clawz-fast&friendly.mod" hasCredits="true" />
+	<song url="./mod/emax-analyzer.mod" hasCredits="true" />
+	<song url="./mod/emax-delicate_oooz!.mod" hasCredits="true" />
+	<song url="./mod/estrayk-giana_sisters.mod" hasCredits="true" />
+	<song url="./mod/estrayk-her4.mod" hasCredits="true" />
+	<song url="./mod/estrayk-thunder.mod" hasCredits="true" />
+	<song url="./mod/gandbox-synthetik2.mod" hasCredits="true" />
+	<song url="./mod/goto8o-dansa_i_neon.mod" hasCredits="true" />
+	<song url="./mod/hollywood-fear_of_the_dark.mod" hasCredits="true" />
+	<song url="./mod/kanewood-level_completed.mod" hasCredits="true" />
+	<song url="./mod/lord_chaos-fred_rape_wilma.mod" hasCredits="true" />
+	<song url="./mod/maktone-class6.mod" hasCredits="true" />
+	<song url="./mod/mattis-lowsi_jeti_maah.mod" hasCredits="true" />
+	<song url="./mod/mr_death-omega_illusion.mod" hasCredits="true" />
+	<song url="./mod/nao-naolytic_adventurse.mod" hasCredits="true" />
+	<song url="./mod/NB_essentials.mod" hasCredits="true" />
+	<song url="./mod/pak-delta.mod" hasCredits="true" />
+	<song url="./mod/pink-afternoon_voyage.mod" hasCredits="true" />
+	<song url="./mod/pink-chip-technics.mod" hasCredits="true" />
+	<song url="./mod/pink-commando_hiscore.mod" hasCredits="true" />
+	<song url="./mod/pink-recruit_the_sonics.mod" hasCredits="true" />
+	<song url="./mod/radix-happy_sundays.mod" hasCredits="true" />
+	<song url="./mod/reed-twentyfour_and_up.mod" hasCredits="true" />
+	<song url="./mod/rez-wavy_song.mod" hasCredits="true" />
+	<song url="./mod/smt-intro007.mod" hasCredits="true" />
+	<song url="./mod/soul-applebomb.mod" hasCredits="true" />
+	<song url="./mod/subject-gatecrashing.mod" hasCredits="true" />
+	<song url="./mod/super_mario_world2.mod" hasCredits="true" />
+	<song url="./mod/technix-complex.mod" hasCredits="true" />
+	<song url="./mod/technix-superlative.mod" hasCredits="true" />
+	<song url="./mod/tempest-free_fjortis_facials.mod" hasCredits="true" />
+	<song url="./mod/vim-not_again.mod" hasCredits="true" />
+	<song url="./mod/wotw-commodore_rulez.mod" hasCredits="true" />
+	<song url="./mod/wotw-jump&run2.mod" hasCredits="true" />
+	<song url="./mod/xtd-microtune.mod" hasCredits="true" />
+</playlist>
